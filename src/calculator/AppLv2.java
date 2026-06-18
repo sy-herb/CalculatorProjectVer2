cd05060df07f05883dd44475e4f29f9ba82fe38c
sy-herb <seoyeon02126@gmail.com>
2026-06-18 11:40:02 +0900

    Step2의 클래스를 따로 구현한 main 코드

---
+++ src/calculator/AppLv2.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class AppLv2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CalculatorStep2 calculator = new CalculatorStep2();
+        String exit = "";
+
+
+        while (!exit.equals("exit")) {
+            System.out.print("첫 번째 정수를 입력하세요. : ");
+            int num1 = sc.nextInt();
+            if (num1 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다.");
+                continue;
+            }
+
+            System.out.print("두 번째 정수를 입력하세요. : ");
+            int num2 = sc.nextInt();
+            if (num2 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다.");
+            }
+
+            System.out.print("연산 기호(+ - * /)를 입력하세요. : ");
+            char oper = sc.next().charAt(0);
+
+            Integer result = calculator.Calculate(num1, num2, oper);
+
+            System.out.println("결과 : " + result);
+
+            System.out.println("현재 저장된 결과 : " + calculator.getResult());
+
+            System.out.print("가장 오래된 결과를 삭제하시겠습니까 ? (yes 입력시 삭제됨)");
+            String remove = sc.next();
+            if (remove.equals("yes")) {
+                calculator.removeResult();
+                System.out.println("현재 남은 결과 : " + calculator.getResult());
+            }
+
+            System.out.print("더 계산하시겠습니까 ? (exit 입력시 종료됨)");
+            exit = sc.next();
+            System.out.println("");
+        }
+        sc.close();
+        System.out.println("프로그램을 종료합니다.");
+    }
+}
